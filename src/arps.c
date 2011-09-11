2abb9bada637543fa91a62d2fe54ecd32c1a63ea
Caleb Carlton <caleb.carlton@gmail.com>
2011-09-11 01:24:16 -0400

    Information needed to build the arp packet is gathered

--- src/arps.c
+++ src/arps.c
@@ -1,5 +1,4 @@
-/* arps
-	Caleb Carlton */
+/* ccarlton */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,9 +9,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <net/if.h>
-#include <pthread.h>
 #include <netinet/in.h>
+#include <linux/if.h>
 #include <getopt.h>
 #include "headers.c"
 
@@ -87,8 +85,8 @@ int main(int argc, char **argv) {
 				break;
 			case 0:
 				/* Child process executes and switches host and target */
-				if ( (packet = arp_type == ARP_REPLY ? build_reply() : build_request()) != 0) { 
-	   			return send_arp(packet);
+				if ( (packet = arp_type == ARP_REPLY ? build_reply(interface, host, target) : build_request()) != 0) { 
+	   			return send_arp(packet, interface);
 				}else {
 					exit(EXIT_FAILURE);	
 				}
@@ -97,13 +95,11 @@ int main(int argc, char **argv) {
 	}
 
 	/* Parent and single process execute this */	
-	if ( (packet = arp_type == ARP_REPLY ? build_reply() : build_request()) != 0){ 
-		return send_arp(packet);
+	if ( (packet = arp_type == ARP_REPLY ? build_reply(interface, target, host) : build_request()) != 0){ 
+		return send_arp(packet, interface);
 	}else {
 	   exit(EXIT_FAILURE);	
 	}
-			
-	
 }
 
 void print_usage() {
@@ -111,16 +107,144 @@ void print_usage() {
 	exit(EXIT_FAILURE);
 }
 
-unsigned char *build_reply() {
+unsigned char *build_reply(char *interface, char *target, char *host) {
+	
+	int sockfd;
+	char *strbuf = calloc(256, 1);
+	FILE *fp;
+	struct ifreq ifr;
+   struct eth_header *eth;
+	struct arp_header *arp;
+	unsigned char *packet = (unsigned char *)calloc(sizeof(struct arp_header) + sizeof(struct eth_header), 1);
+
+
+   struct sockaddr_in *ip_addr;
+   struct in_addr addr;
+   char target_mac[18];
+   char src_ip[4];
+   char *src_ip_str;
+
+	
+	/* Create a datagram socket to find interface IP */
+   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      perror("sockfd");
+      exit(EXIT_FAILURE);
+   }
+
+   /* Set our ifr struct family to IPV4 */
+   ifr.ifr_addr.sa_family = AF_INET;
+   /* Copy interface name to ifr_name */
+   strncpy(ifr.ifr_name, interface, IFNAMSIZ-1);
+
+   /* IOCTL:
+      SIOGIFADDR gets an interface addr
+      SIOCSIFADDR sets an interface addr
+      SIOCGHWADDR gets locak HW addr
+    */
+   if (ioctl(sockfd, SIOCGIFADDR, (char *)&ifr) <  0) {
+      perror("icotl");
+      exit(EXIT_SUCCESS);
+   }
+
+   ip_addr = (struct sockaddr_in*)&ifr.ifr_addr;
+   memcpy(src_ip, &ip_addr->sin_addr, 4);
+   src_ip_str = inet_ntoa(ip_addr->sin_addr);
+
+   if (ioctl(sockfd, SIOCGIFHWADDR, (char *)&ifr) < 0) {
+      perror("ioctl");
+      exit(EXIT_SUCCESS);
+   }
+
+	close(sockfd);
+
+	send_ping(target);
+
+	/* Get info from /proc/net/arp */
+   if ((fp = fopen("/proc/net/arp", "r")) == 0) {
+      fprintf(stderr, "Error opening /proc/net/arp");
+      exit(EXIT_FAILURE);
+   }
+
+   /* Search for the arp entry containing our target ip */
+	strcat(target, " ");
+	while (fgets(strbuf, 256, fp) != NULL) {
+		if (strstr(strbuf, target) != 0) {
+         /* Parse out fields */
+         strncpy(target_mac, strstr(strbuf, ":")-2, 18);
+			break;
+      }
+   }
+	target[strlen(target)-1] = 0;
+	
+	if(!inet_pton(AF_INET, target, &addr)) {
+      fprintf(stderr, "Error converting IP Address\n");
+      exit(EXIT_FAILURE);
+   }
+
+	eth = (struct eth_header *)packet;
+	arp = (struct arp_header *)(packet + sizeof(struct eth_header));
+
+
+   /*
+   memcpy(args->dip,  &addr.s_addr,  4);
+   memcpy(args->sip,  src_ip, 4);
+   memcpy(args->dmac, formatConvertMac(target_mac), 6);
+   memcpy(args->smac, ifr.ifr_hwaddr.sa_data, 6);
+	*/
+
+	if(DEBUG) {
+      printf("Target Mac: %.14s\n", target_mac);
+      printf("Source Mac: ");
+      print_mac((unsigned char *)ifr.ifr_hwaddr.sa_data);
+      printf("Network address: %s\n", src_ip_str);
+      printf("Target address: %s\n\n", target);
+   }
+
+	return 0;
+}
+
+int send_ping(char *target) {
+	char *strbuf = (char *)calloc(256,1);
+
+	/* Send Ping/Subnet check */
+   strcpy(strbuf, "ping -c 1 ");
+   strcat(strbuf, target);
+   strcat(strbuf, " > /dev/null");
+
+   if (system(strbuf) != 0) {
+      fprintf(stderr, "Error executing ping system call\n");
+      exit(EXIT_FAILURE);
+   }
 	
 	return 0;
+
 }
 
 unsigned char *build_request() {
 	return 0;
 }
 
-int send_arp(unsigned char *packet) {
+int send_arp(unsigned char *packet, char *interface) {
+
+	struct sockaddr sa;
+	int sockfd;
+	   /* Raw socket to send */
+   if ((sockfd = socket(AF_INET, SOCK_PACKET, htons(ETH_P_RARP))) < 0) {
+      perror("socket");
+      exit(EXIT_FAILURE);
+   }
+
+   if (DEBUG) {
+      print_buf((unsigned char *)packet, sizeof(struct eth_header) + sizeof(struct arp_header));
+   }
+
+   strcpy(sa.sa_data, interface);
+   if(sendto(sockfd, packet, sizeof(struct eth_header) + sizeof(struct arp_header), 0, &sa, sizeof(sa)) < 0) {
+      perror("Error sending packet");
+      exit(EXIT_FAILURE);
+   }
+
+		
 	return 0;
 }
 
